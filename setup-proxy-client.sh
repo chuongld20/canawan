0f541656e22bac199c60ee54b7403c1be0d476c4
chuongld20 <128578760+chuongld20@users.noreply.github.com>
2025-02-24 19:06:03 +0700

    Update setup-proxy-client.sh

--- setup-proxy-client.sh
+++ setup-proxy-client.sh
@@ -44,10 +44,12 @@ sudo systemctl disable proxy-client 2>/dev/null || true
 timeout 10 pkill -9 -f BigCat.Proxy.Client || echo "⚠️ Process not found or timeout reached."
 sudo ip -6 addr flush dev ens6
 sleep 5
-sudo ip link set ens6 down
-echo "down ens6"
-sleep 5
-sudo ip link set ens6 up
+
+netplan apply
+# sudo ip link set ens6 down
+# echo "down ens6"
+# sleep 5
+# sudo ip link set ens6 up
 
 echo "🔹 Creating new systemd service..."
 SERVICE_FILE="/etc/systemd/system/proxy-client.service"
@@ -57,47 +59,47 @@ cd $INSTALL_DIR
 
 nohup ./BigCat.Proxy.Client --defaultServerEndPointIP=$PUBLIC_IP --portAPI=9000 --passwordAPI=66778899 --defaultPortIPv4=9010 --maxConnections=-1 --maxConnectionPerCredential=-1 --networkInterface=ens6 --ipV6RotationSeconds=-1 --fromPort=20000 --toPort=30000 --autoOffAllFirewall=true     --autoConfigPortFirewall=false     --showFullDebug=true > /var/log/proxy-client.log 2>&1 &
 
-sudo tee "$SERVICE_FILE" > /dev/null <<EOF
-[Unit]
-Description=Proxy Client Service
-After=network.target
+# sudo tee "$SERVICE_FILE" > /dev/null <<EOF
+# [Unit]
+# Description=Proxy Client Service
+# After=network.target
 
-[Service]
-Environment="LD_LIBRARY_PATH=/config/proxy-service/client"
-User=root
-WorkingDirectory=$INSTALL_DIR
-ExecStartPre=/bin/chmod +x $INSTALL_DIR/BigCat.Proxy.Client
-ExecStart=cd $INSTALL_DIR && ./BigCat.Proxy.Client \
-    --defaultServerEndPointIP=$PUBLIC_IP \
-    --portAPI=9000 \
-    --passwordAPI=66778899 \
-    --defaultPortIPv4=9010 \
-    --maxConnections=-1 \
-    --maxConnectionPerCredential=-1 \
-    --networkInterface=ens6 \
-    --ipV6RotationSeconds=-1 \
-    --fromPort=20000 \
-    --toPort=30000 \
-    --autoOffAllFirewall=true \
-    --autoConfigPortFirewall=false \
-    --showFullDebug=false
-Restart=always
-StandardOutput=append:/var/log/proxy-client.log
-StandardError=append:/var/log/proxy-client-error.log
+# [Service]
+# Environment="LD_LIBRARY_PATH=/config/proxy-service/client"
+# User=root
+# WorkingDirectory=$INSTALL_DIR
+# ExecStartPre=/bin/chmod +x $INSTALL_DIR/BigCat.Proxy.Client
+# ExecStart=cd $INSTALL_DIR && ./BigCat.Proxy.Client \
+#     --defaultServerEndPointIP=$PUBLIC_IP \
+#     --portAPI=9000 \
+#     --passwordAPI=66778899 \
+#     --defaultPortIPv4=9010 \
+#     --maxConnections=-1 \
+#     --maxConnectionPerCredential=-1 \
+#     --networkInterface=ens6 \
+#     --ipV6RotationSeconds=-1 \
+#     --fromPort=20000 \
+#     --toPort=30000 \
+#     --autoOffAllFirewall=true \
+#     --autoConfigPortFirewall=false \
+#     --showFullDebug=false
+# Restart=always
+# StandardOutput=append:/var/log/proxy-client.log
+# StandardError=append:/var/log/proxy-client-error.log
 
-[Install]
-WantedBy=multi-user.target
-EOF
+# [Install]
+# WantedBy=multi-user.target
+# EOF
 
 sleep 5
 
-sudo chmod 644 $SERVICE_FILE
+# sudo chmod 644 $SERVICE_FILE
 
 echo "🔹 Reloading systemd..."
-sudo systemctl daemon-reload
+# sudo systemctl daemon-reload
 
 echo "🔹 Setting execute permissions..."
-sudo chmod +x /config/proxy-service/client/BigCat.Proxy.Client
+# sudo chmod +x /config/proxy-service/client/BigCat.Proxy.Client
 
 echo "🔹 Enabling and starting service..."
 # sudo systemctl enable --now proxy-client
